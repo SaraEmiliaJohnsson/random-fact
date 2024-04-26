7147b7202e787dc8d792f0a9d22fb0164b1332eb
Sara Johnsson <sara86persson@hotmail.com>
2024-04-26 10:07:20 +0200

    added fetch with api

--- random-fact/src/App.tsx
+++ random-fact/src/App.tsx
@@ -5,12 +5,26 @@ import './App.css'
 
 function App() {
 
+  fetchFact();
 
   return (
     <>
 
     </>
   )
+}
+
+async function fetchFact() {
+
+  const URL = 'https://uselessfacts.jsph.pl/api/v2/facts/random?language=en';
+
+  const response = await fetch(URL);
+  const json = await response.json();
+
+  const randomFact = json.text;
+
+  console.log(randomFact);
+
 }
 
 export default App
