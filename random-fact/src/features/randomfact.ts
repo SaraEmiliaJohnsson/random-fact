8da77ad1bf62b795d6491d57a6cd36b8547cad27
Sara Johnsson <sara86persson@hotmail.com>
2024-04-26 12:14:09 +0200

    added reducersin redux

--- random-fact/src/features/randomfact.ts
+++ random-fact/src/features/randomfact.ts
@@ -1,2 +1,35 @@
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
+enum Status {
+    NORMAL,
+    FETCHING,
+    SUCCESS,
+    FAILURE
+}
+
+interface Fact {
+    fact: string | null;
+    status: Status;
+}
+
+const initialState: Fact = {
+    fact: null,
+    status: Status.NORMAL
+}
+
+const isfetching = createAction('is fetching fact');
+const success = createAction<string>('success fact');
+const failure = createAction('failure fact');
+
+const fatchActions = { isfetching, success, failure }
 
 
+const randomFactReducer = createReducer(initialState, builder => {
+    builder
+        .addCase(isfetching, (state, action) => ({
+            status: Status.FETCHING,
+            fact: state.fact
+        }))
+})
+
+export { fatchActions, randomFactReducer }
